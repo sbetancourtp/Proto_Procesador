e9ff7daf6e6ad8e35be0df9ce2d216d2e841d6eb
sbetancourtp <sbetancoutp@unal.edu.co>
2019-09-27 14:07:07 -0500

    update: banco de registros terminado (sin simular)

--- parcial/registros/regs.v
+++ parcial/registros/regs.v
@@ -1,46 +1,72 @@
-module regs#(parameter DATASIZE = 8) (clk, rst, instr, data_in, data_out);
+module regs#(parameter DATASIZE = 8) (main_enable, clk, rst, instr, data_in, out_A, out_B);
 
+input main_enable;
 input clk;
 input rst;
 input [4:0] data_in;
 input [7:0] instr;
-output [DATASIZE - 1: 0] data_out;
+output reg [DATASIZE - 1: 0] out_A;
+output reg [DATASIZE - 1: 0] out_B;
 
-[3:0] registers [1:0];
+reg [3:0] registers [1:0];
 
-//always para lectura de datos
+//always principal
 
 always @(posedge clk) begin
-    case (instr[7:5])
-        0:  begin
-                if (instr[0] == 0) begin
-                    registers [0] = data_in;
-                end else begin
-                    registers [1] = data_in;
-                end
+    
+    if (rst) begin
+        registers[0] = 0;
+        registers[1] = 0;
+    end
 
-            end
-        1: ;
-        2: ;
-        3: ;
-        4: ;
-        5: ;
-        6: begin    if (data_in[4] == 0) begin
+    if (main_enable) begin
+        case (instr[7:5])
+            0:  begin
+                    if (instr[4] == instr[3]) begin
+                        out_A = registers[0];
+                        out_B = registers[1];
+                    end else begin
+                        if (instr[4] == 0) begin
+                            out_A = registers[0];
+                        end else begin
+                            out_A = registers[1];
+                        end
+
+                        if (instr[3] == 0) begin
+                            out_B = registers[0];
+                        end else begin
+                            out_B = registers[1];
+                        end
+                    end
+
+                    if (instr[2] == 0) begin
+                        registers[0] = data_in;
+                    end else begin
+                        registers[1] = data_in;
+                    end
+                end
+    //       1: ;
+    //       2: ;
+    //       3: ;
+    //       4: ;
+    //       5: ;
+            6:  begin
+                    if (data_in[4] == 0) begin
                         registers [0] = data_in[3:0];
                     end else begin
                         registers [1] = data_in[3:0];
                     end
-            end
-        7: ;
-        default: ;
-    endcase
+                end
+    //       7: ;
+            default: begin registers [0] = 0; registers [1] = 0; end
+        endcase
+    end
 end
 
-// always para escritura de datos
-// los registros deben enviar datos, entonces, cuando el modulo
-// esté en enviar datos (read), se debe declarar que las salidas
-// del sistema se envíen a los respectivos outputs
+/*
+issues: tener claro cómo es el uso de las igualdades síncronas y asíncronas
+no está muy claro cómo es el uso de los de los if's para lectura y escritura en el sumador
 
-always @(posedge clk) begin
-    data_out == 
-end
+*/
+
+endmodule
